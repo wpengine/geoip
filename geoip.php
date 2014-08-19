58a58a8e23664df7fe036bc0bb62bb7abf32ac4f
pod-9999 <no-reply@wpengine.com>
2014-08-19 22:42:07 +0000

    0.2 refactor for singleton invocation

--- geoip.php
+++ geoip.php
@@ -1,25 +1,46 @@
 <?php
 /*
  * Plugin Name: WP Engine Geo
- * Version: 0.1
+ * Version: 0.2
  * Author: WP Engine Labs
+ *
+ * Examples use of how to add geoip information to post content:
+
+function append_content($content) {
+	$geo = Geoip::instance();
+?> How's the weather in <?=$geo->city()?>, <?=$geo->region()?> <?=$geo->country()?>?</br></br> <?php
+}
+add_filter('the_content', 'WPEngine\append_content' );
+
 */
 
 namespace WPEngine;
 
 class GeoIp {
 
+	// The single instance of this object.  No need to have more than one.
+	private static $instance = null;
+
+	// The geographical data loaded from the environment
 	public $geos;
 
-	public function __construct() {
-		$actuals = $this->get_actuals();
-		$overrides = $this->get_overrides();
-		$this->geos = array_merge($actuals, $overrides);
+	public static function init() {
+		add_action('plugins_loaded', array(self::instance(), 'setup'));
+	}
+
+	public static function instance() {
+		// create a new object if it doesn't exist.
+		is_null(self::$instance) && self::$instance = new self;
+		return self::$instance;
+	}
+
+	public function setup() {
+		$this->geos = $this->get_actuals();
 	}
 
 	/**
- 	* here we extract the data from headers set by nginx -- lets only send them if they are part of the cache key
- 	*/
+ 	 * here we extract the data from headers set by nginx -- lets only send them if they are part of the cache key
+ 	 **/
 	public function get_actuals() {
 		return array(
 			'countrycode' => getenv('HTTP_GEOIP_COUNTRY_CODE'),
@@ -34,14 +55,9 @@ class GeoIp {
 		);
 	}
 
-	public function get_overrides() {
-		return array(
-		);
-	}
-
 	/**
- 	* examples of easy to use utility functions that we should have for each geo that is part of the cache key
- 	*/
+ 	 * examples of easy to use utility functions that we should have for each geo that is part of the cache key
+ 	 **/
 	public function country() {
 		return $this->geos['countrycode'];
 	}
@@ -57,12 +73,4 @@ class GeoIp {
 /**
  * Register to do the stuff
  */
-function append_content($content) {
-	print date('r')."<br>";
-	$geo = new GeoIp();
-	print "Country: ".$geo->country()."<br>";
-	print "Region: ".$geo->region()."<br>";
-	print "City: ".$geo->city()."<br>";
-}
-add_filter('the_content', 'WPEngine\append_content' );
-
+GeoIp::init();
---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== Plugin Name ===
+Contributors: markkelnar
+Requires at least: 3.0.1
+Tested up to: 3.9.2
+Stable tag: 0.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Here is a short description of the plugin.  This should be no more than 150 characters.  No markup here.
+
+== Description ==
+
+== Installation ==
+
+1. Upload `geoip` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+== Changelog ==
+
+= 0.2 =
+- Add static function and singleton construction.
+
+= 0.1 =
+- Initial version
