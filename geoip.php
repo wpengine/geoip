4fbcc83ff5b9e2df594408f22ed0167eddb98272
Josh Eaton <josh@josheaton.org>
2014-11-24 15:24:26 -0500

    Fix incorrect class reference in example.

--- geoip.php
+++ geoip.php
@@ -7,7 +7,7 @@
  * Examples use of how to add geoip information to post content:
 
 function geoip_append_content( $content ) {
-	$geo = Geoip::instance();
+	$geo = WPEngine\GeoIp::instance();
 ?> How's the weather in <?=$geo->city()?>, <?=$geo->region()?> <?=$geo->country()?>?</br></br> <?php
 }
 add_filter( 'the_content', 'geoip_append_content' );
