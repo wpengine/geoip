33c416c449780bbdf1c0d6b626de1768a6cd5360
Nate Gay <nate.gay@wpengine.com>
2018-02-27 01:49:09 -0600

    Add smoke test for plugin activation, add phpunit and integration test bootstrap.

---
+++ tests/integration-bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Create test bootstrap
+ *
+ * @package wpengine-geoip
+ */
+
+$_tests_dir = '/tmp/wordpress-tests-lib';
+
+/**
+ * The WordPress tests functions.
+ *
+ * We are loading this so that we can add our tests filter
+ * to load the plugin, using tests_add_filter().
+ */
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin main file.
+ *
+ * The plugin won't be activated within the test WP environment,
+ * that's why we need to load it manually.
+ *
+ * You will also need to perform any installation necessary after
+ * loading your plugin, since it won't be installed.
+ */
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../class-geoip.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+/**
+ * Sets up the WordPress test environment.
+ *
+ * We've got our action set up, so we can load this now,
+ * and viola, the tests begin.
+ */
+require $_tests_dir . '/includes/bootstrap.php';
