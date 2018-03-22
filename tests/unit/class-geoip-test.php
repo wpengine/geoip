6ae205087de1ddacd64cf04b3edd98639d4c5b51
Nate Gay <nate.gay@wpengine.com>
2018-03-21 23:40:33 -0500

    Adding test for #39

--- tests/unit/class-geoip-test.php
+++ tests/unit/class-geoip-test.php
@@ -29,4 +29,25 @@ class GeoIp_Test extends \WP_UnitTestCase {
 		$this->assertEquals( $this->class_name, get_class( $geo ) );
 	}
 
+	/**
+	 * Test display of admin notice
+	 */
+	public function test_action_admin_notices() {
+		$this->markTestSkipped( 'Skip can be removed after #39 is merged.' );
+
+		$geo = new $this->class_name();
+		$geo->setup();
+		$geo->action_admin_init_check_plugin_dependencies();
+		$geo->action_admin_notices();
+
+		$output = join('', array(
+			'<div class="notice notice-warning wpengine-geoip is-dismissible" data-key="dependency">',
+			'<p>',
+			'WP Engine GeoIP requires a <a href="http://wpengine.com/plans/?utm_source=wpengine-geoip">WP Engine account</a> with GeoIP enabled for full functionality. Only testing queries will work on this site.',
+			'</p>',
+			'</div>',
+		));
+
+		$this->expectOutputString( $output );
+	}
 }
