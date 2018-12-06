c3b01131f9b0c77d6b0a1a4582a886b2c457f77d
Nate Gay <email@nategay.me>
2018-12-06 00:01:34 -0600

    fix lint issues

--- src/wpengine-geoip.php
+++ src/wpengine-geoip.php
@@ -13,13 +13,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Replace plugin filename in options table
+ */
 function replace_previous_plugin_filename() {
-    $active_plugins = get_option( 'active_plugins', array() );
-    foreach ( $active_plugins as $key => $active_plugin ) {
-        if ( strstr( $active_plugin, '/wpengine-geoip.php' ) ) {
-            $active_plugins[ $key ] = str_replace( '/wpengine-geoip.php', '/class-geoip.php', $active_plugin );
-        }
-    }
-    update_option( 'active_plugins', $active_plugins );
+	$active_plugins = get_option( 'active_plugins', array() );
+	foreach ( $active_plugins as $key => $active_plugin ) {
+		if ( strstr( $active_plugin, '/wpengine-geoip.php' ) ) {
+			$active_plugins[ $key ] = str_replace( '/wpengine-geoip.php', '/class-geoip.php', $active_plugin );
+		}
+	}
+	update_option( 'active_plugins', $active_plugins );
 }
 replace_previous_plugin_filename();
